3dc825aa256a4314927d1ce338c53c447ba08910
James Liao <s9446760133@gmail.com>
2022-02-28 21:40:03 +0800

    add

---
+++ copy_model.sh
@@ -0,0 +1,7 @@
+ssh -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble1/model_2.pt ./outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble1/
+ssh -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble2/model_2.pt ./outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble2/
+ssh -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble3/model_2.pt ./outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble3/
+ssh -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble4/model_2.pt ./outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble4/
+ssh -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble5/model_2.pt ./outputs_models/cross_encoder_20_40_keywords_title_text_large_ensemble5/
+
+
