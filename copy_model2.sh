03a12eeb9d5292972e5a6ccda365581ca59c91f0
James Liao <s9446760133@gmail.com>
2022-02-28 21:58:41 +0800

    add

--- copy_model2.sh
+++
@@ -1,5 +0,0 @@
-scp -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_keywords_title_text_large_ensemble1/model_2.pt ./outputs_models/
-scp -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_keywords_title_text_large_ensemble2/model_2.pt ./outputs_models/
-scp -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_keywords_title_text_large_ensemble3/model_2.pt ./outputs_models/
-scp -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_keywords_title_text_large_ensemble4/model_2.pt ./outputs_models/
-scp -P 2222 harry@venom.nlplab.cc:~/data_mining/final_project/outputs_models/cross_encoder_keywords_title_text_large_ensemble5/model_2.pt ./outputs_models/
